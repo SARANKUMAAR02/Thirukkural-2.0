cff5e99461ccafba10ba8d2352fe843087698eae
SRSARANKUMAAR02 <srsarankumaar262003@gmail.com>
2023-08-21 21:57:19 +0530

    Updates

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-let div=document.createElement('div')
-let text=document.createElement('input')
-text.id="txt"
-text.placeholder='TYPE THIRUKKURAL NO'
-text.style.width="250px";text.style.borderColor="red";text.style.padding="2px"
-let wrapper=document.querySelector('#wrapper')
-let btn=document.createElement('button')
-div.appendChild(text)
-div.appendChild(btn)
-div.style.paddingTop='50px'
-div.style.paddingLeft='30%'
-wrapper.appendChild(div)
-btn.textContent="Search"
-btn.style.cursor='pointer'
+
+let text=document.getElementById('txt')
+let btn=document.getElementById('btn')
+
 btn.addEventListener('click',Thirukkural)
 
 
@@ -20,6 +10,7 @@ function Thirukkural(){
     let a=Number(document.getElementById('txt').value)
     fetch('https://api-production-f819.up.railway.app/work/thiru/api?id='+a)
     .then((res)=>{
+        console.log(res)
         if(res.ok){
             console.log('success')
         }
@@ -28,50 +19,19 @@ function Thirukkural(){
         }
         return res.json()})
     .then((msg)=>{
-        document.getElementById('line1').innerHTML=msg[0].line1;
-        document.getElementById('line2').innerHTML=msg[0].line2;
+        let line1=msg[0].line1;
+        let line2=msg[0].line2;
+        document.getElementById('line1').innerHTML=line1;
+        document.getElementById('line2').innerHTML=line2;
         document.getElementById('tit').innerHTML=msg[0].chap_tam;
+        responsiveVoice.speak(`${line1+line2}`, "Tamil Male",{rate: 0.8});
         document.getElementById('auth').innerHTML="-திருவள்ளுவர்"
     })
     .catch((err)=>{
         document.getElementById('auth').innerHTML=""
         document.getElementById('line1').innerHTML=`CHECK YOUR NETWORK CONNECTION (OR)`;
-        document.getElementById('line2').innerHTML=`குறள் COUNT(1330) EXCEEDED`;
+        document.getElementById('line2').innerHTML=`I HAVE ADDED ONLY 10 குறள்`;
 
     })
     
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* fetch("http://localhost:4000/work/thiru/api?id=1")
-.then((msg)=>{
-
-    console.log(msg.json())
-    return msg.json()
-})
-.then((res)=>{
-    let h5=document.getElementById('h5')
-    
-    h5.innerHTML=res.line1
-})
-.catch((err)=>{
-    console.log(err)
-})  */
+} 
