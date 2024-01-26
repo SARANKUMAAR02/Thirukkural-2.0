9da1f52944d3b425037ab28072b1c106ab3fea83
SRSARANKUMAAR02 <srsarankumaar262003@gmail.com>
2024-01-26 17:43:54 +0530

    feat : speech recognition

--- script.js
+++ script.js
@@ -2,6 +2,44 @@
 let text=document.getElementById('txt')
 let btn=document.getElementById('btn')
 
+var microphone = document.getElementById('microphone');
+microphone.onclick = function () {
+	
+	microphone.classList.add("record");
+	var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+	recognition.lang = 'en-US';
+	recognition.start();
+
+
+	numbers = {
+		"zero":0,
+		"one":1,
+		"two":2,
+		"three":3,
+		"four":4,
+		"five":5,
+		"six":6,
+		"seven":7,
+		"eight":8,
+		"nine":9
+	}
+
+	recognition.onresult = function (event) {
+		console.log(event)
+		var input = event.results[0][0].transcript;
+		for (property in numbers) {
+			
+			input = input.replace(property, numbers[property]);
+			
+		}
+
+		document.getElementById("txt").value = input;
+        Thirukkural()
+		microphone.classList.remove("record");
+	}
+
+}
+
 btn.addEventListener('click',Thirukkural)
 
 
